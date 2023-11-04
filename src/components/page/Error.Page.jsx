63c24f8c8945ed4562d4192ac2f990e2701f7b3d
Jin <genesisjinsaquibal@gmail.com>
2023-11-04 19:27:00 +0800

    centralized error page

--- src/components/page/Error.Page.jsx
+++ src/components/page/Error.Page.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import Error500View from "../views/Error500.View";
 import Error503View from "../views/Error503.View";
+import Error404View from "../views/Error404.View";
 
 export default function ErrorPage({ error, resetErrorBoundary }) {
+  if (error.code === "404") {
+    return (
+      <div className="flex items-center justify-center min-h-screen text-gray-900 border">
+        <Error404View />
+      </div>
+    );
+  }
+
+  if (error.code === "ERR_NETWORK") {
+    <div className="flex items-center justify-center min-h-screen text-gray-900 border">
+      <Error503View resetErrorBoundary={resetErrorBoundary} />
+    </div>;
+  }
+
   return (
     <div className="flex items-center justify-center min-h-screen text-gray-900 border">
-      {error.code === "ERR_NETWORK" ? (
-        <Error503View resetErrorBoundary={resetErrorBoundary} />
-      ) : (
-        <Error500View resetErrorBoundary={resetErrorBoundary} />
-      )}
+      <Error500View resetErrorBoundary={resetErrorBoundary} />
     </div>
   );
 }
