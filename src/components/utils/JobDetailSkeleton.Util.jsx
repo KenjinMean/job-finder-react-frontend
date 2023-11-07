54b163be7bf4c9f8a333e7b228b378e2eca00b05
Jin <genesisjinsaquibal@gmail.com>
2023-11-07 18:21:57 +0800

    title: added framer motion animation

--- src/components/utils/JobDetailSkeleton.Util.jsx
+++ src/components/utils/JobDetailSkeleton.Util.jsx
@@ -1,35 +1,47 @@
 import React from "react";
-import JobListSkeletonUtil from "./JobListSkeleton.Util";
+
+import { motion } from "framer-motion";
 import MaxWidthWrapperUtil from "./MaxWidthWrapper.Util";
 
 export default function JobDetailSkeletonUtil() {
+  const containerAnimation = {
+    initial: { opacity: 0, y: 50 },
+    animate: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  };
+
   return (
     <MaxWidthWrapperUtil className="p-5 rounded rouned-md">
-      <div className="flex flex-col gap-5">
-        <div className="flex flex-col w-full gap-2">
-          <div className="w-1/3 h-10 rounded-md pulse"></div>
-          <div className="w-1/2 h-3 rounded-md pulse"></div>
-          <div className="w-3/5 h-3 rounded-md pulse"></div>
-          <div className="w-1/2 h-3 rounded-md pulse"></div>
+      <motion.div
+        initial="initial"
+        animate="animate"
+        variants={containerAnimation}
+      >
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col w-full gap-2">
+            <div className="w-1/3 h-10 rounded-md pulse"></div>
+            <div className="w-1/2 h-3 rounded-md pulse"></div>
+            <div className="w-3/5 h-3 rounded-md pulse"></div>
+            <div className="w-1/2 h-3 rounded-md pulse"></div>
+          </div>
+          <div className="flex flex-col w-full gap-2">
+            <div className="w-1/3 h-10 rounded-md pulse"></div>
+            <div className="w-1/2 h-3 rounded-md pulse"></div>
+            <div className="w-1/2 h-3 rounded-md pulse"></div>
+          </div>
+          <div className="flex w-full gap-5">
+            <div className="w-10 h-5 rounded-full pulse"></div>
+            <div className="w-10 h-5 rounded-full pulse"></div>
+            <div className="w-10 h-5 rounded-full pulse"></div>
+            <div className="w-10 h-5 rounded-full pulse"></div>
+            <div className="w-10 h-5 rounded-full pulse"></div>
+          </div>
+          <div className="flex flex-col w-full gap-2">
+            <div className="w-1/3 h-10 rounded-md pulse"></div>
+            <div className="w-1/2 h-3 rounded-md pulse"></div>
+            <div className="w-1/2 h-3 rounded-md pulse"></div>
+          </div>
         </div>
-        <div className="flex flex-col w-full gap-2">
-          <div className="w-1/3 h-10 rounded-md pulse"></div>
-          <div className="w-1/2 h-3 rounded-md pulse"></div>
-          <div className="w-1/2 h-3 rounded-md pulse"></div>
-        </div>
-        <div className="flex w-full gap-5">
-          <div className="w-10 h-5 rounded-full pulse"></div>
-          <div className="w-10 h-5 rounded-full pulse"></div>
-          <div className="w-10 h-5 rounded-full pulse"></div>
-          <div className="w-10 h-5 rounded-full pulse"></div>
-          <div className="w-10 h-5 rounded-full pulse"></div>
-        </div>
-        <div className="flex flex-col w-full gap-2">
-          <div className="w-1/3 h-10 rounded-md pulse"></div>
-          <div className="w-1/2 h-3 rounded-md pulse"></div>
-          <div className="w-1/2 h-3 rounded-md pulse"></div>
-        </div>
-      </div>
+      </motion.div>
     </MaxWidthWrapperUtil>
   );
 }
