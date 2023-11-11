83d6f916656677ab3dd116735df3f71c2fc13e01
Jin <genesisjinsaquibal@gmail.com>
2023-11-11 22:03:57 +0800

    title: moved custom hooks under lib directory

--- src/lib/hooks/useJobRequestHandler.js
+++ src/lib/hooks/useJobRequestHandler.js
@@ -1,4 +1,4 @@
-import axiosClient from "../axios-client";
+import axiosClient from "../../axios-client";
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 
 const getJobDetails = (id) => {
