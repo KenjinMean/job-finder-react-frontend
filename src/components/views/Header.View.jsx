78b3f8b232c46c2ac71d80c9b773e3b34fb05f9e
Jin <genesisjinsaquibal@gmail.com>
2023-11-13 21:43:03 +0800

    title: removed modal testings

--- src/components/views/Header.View.jsx
+++ src/components/views/Header.View.jsx
@@ -8,10 +8,8 @@ import HeaderNavViewV2 from "./HeaderNavV2.View";
 import { useStateContext } from "../../context/ContextProvider";
 import { useRefreshToken } from "../../lib/hooks/useAuthRequestHandler";
 
-import OverlaysUtil from "../../utils/Overlays.Util";
 import MaxWidthWrapperUtil from "../../utils/MaxWidthWrapper.Util";
 
-import TestModal from "../modals/user/Test.Modal";
 import AutoCompleteSearchBarUiComponent from "../UI/AutoCompleteSearchBar.Ui.Component";
 
 export default function HeaderView() {
@@ -52,8 +50,6 @@ export default function HeaderView() {
     }
   }, [token]);
 
-  const { openModal } = useModalStates((state) => state);
-
   return (
     <header className="p-5 py-8 bg-gradient-to-r from-background-100 to-background-200 lg:px-10">
       <MaxWidthWrapperUtil>
@@ -62,14 +58,6 @@ export default function HeaderView() {
           <Fragment>
             <HeroBannerView />
             <AutoCompleteSearchBarUiComponent />
-            <button
-              className="px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear bg-pink-500 rounded shadow outline-none active:bg-pink-600 hover:shadow-lg focus:outline-none"
-              type="button"
-              onClick={() => openModal("testModalState")}
-            >
-              Open regular modal
-            </button>
-            <OverlaysUtil modalComponent={<TestModal />} />
           </Fragment>
         )}
       </MaxWidthWrapperUtil>
