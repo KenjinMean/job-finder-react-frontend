d63d8ce40b2d67afe1df511262b1fb913c04ce19
Jin <genesisjinsaquibal@gmail.com>
2023-11-22 11:00:04 +0800

    title: added comment to fix later

--- src/components/utils/QueryBoundaries.Util.jsx
+++ src/components/utils/QueryBoundaries.Util.jsx
@@ -1,5 +1,4 @@
 // SOURCE: https://www.bam.tech/en/article/suspense-and-react-query-make-data-fetching-easy
-
 import React, { Suspense } from "react";
 
 import { ErrorBoundary } from "react-error-boundary";
@@ -7,6 +6,8 @@ import { QueryErrorResetBoundary } from "@tanstack/react-query";
 
 import ErrorPage from "../../pages/Error.Page";
 import JobListSkeletonUtil from "./LoadersSpinners/JobListSkeleton.Util";
+
+// FIX: is suspense needed here?
 
 export const QueryBoundaries = ({ children }) => (
   <QueryErrorResetBoundary>
