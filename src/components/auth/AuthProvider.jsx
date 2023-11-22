2a0d06e766193e5144941322e0aee7919768e61f
Jin <genesisjinsaquibal@gmail.com>
2023-11-22 10:59:43 +0800

    title: created authProvider

---
+++ src/components/auth/AuthProvider.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef } from "react";
+import { useRefreshToken } from "../../lib/hooks/ApiRequestsHandlers/useAuthRequestHandler";
+import { useStateContext } from "../../context/ContextProvider";
+
+const AuthProvider = ({ children }) => {
+  const isRefreshing = useRef(false);
+
+  const { token, user, setToken, refreshTimeoutRef, setIsTokenRefreshing } =
+    useStateContext();
+
+  const refreshTokenSuccess = (data) => {
+    refreshTimeoutRef.current = setTimeout(() => {
+      refreshTokenFn();
+    }, (data.expires_in - 5) * 1000);
+
+    setToken(data);
+  };
+
+  const refreshTokenFinally = () => setIsTokenRefreshing(false);
+
+  const { refetch: refreshTokenFn } = useRefreshToken(
+    user.id,
+    refreshTokenSuccess,
+    refreshTokenFinally
+  );
+
+  const refreshToken = () => {
+    if (!isRefreshing.current) {
+      setIsTokenRefreshing(true);
+      isRefreshing.current = true;
+      refreshTokenFn();
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      refreshToken();
+    }
+  }, [token]);
+
+  return <>{children}</>;
+};
+
+export default AuthProvider;
