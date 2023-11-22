0272ccc1a7c5a94b104aafc661cc85491dfc5d69
Jin <genesisjinsaquibal@gmail.com>
2023-11-22 10:59:27 +0800

    title: added queryBoundaries and authProvider

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { ErrorBoundary } from "react-error-boundary";
 import { ContextProvider } from "./context/ContextProvider";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./components/auth/AuthProvider";
+import { QueryBoundaries } from "./components/utils/QueryBoundaries.Util";
 
 const queryClient = new QueryClient();
 
@@ -13,7 +15,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ContextProvider>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <QueryBoundaries>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </QueryBoundaries>
         <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
       </QueryClientProvider>
     </ContextProvider>
