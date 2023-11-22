b34ccd6fbf55acdcc968319e4c25fb45856aa34c
Jin <genesisjinsaquibal@gmail.com>
2023-11-22 10:54:46 +0800

    title: moved and deleted components

--- src/components/user/UserProfile.View.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default function UserProfileView() {
-  // pu tthe profile page content here and render this component tin the profile page
-  return <div>UserProfile.View</div>;
-}
--- src/components/views/error/Error409.View.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Error409View() {
-  return <div>Error409.View</div>;
-}
