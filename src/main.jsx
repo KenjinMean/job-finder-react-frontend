67a58114a93b3ac29be959465e2e46bc44d0d03b
Jin <genesisjinsaquibal@gmail.com>
2023-11-23 21:40:01 +0800

    Move: moved AuthProvider component to provider directory This component is being moved to the providers directory as it now serves a role similar to a provider, managing state or providing context for its children components.

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "./components/auth/AuthProvider";
 import { QueryBoundaries } from "./components/utils/QueryBoundaries.Util";
+import AuthProviderProvider from "./services/providers/AuthProvider.Provider";
 
 const queryClient = new QueryClient();
 
@@ -16,9 +17,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <ContextProvider>
       <QueryClientProvider client={queryClient}>
         <QueryBoundaries>
-          <AuthProvider>
+          <AuthProviderProvider>
             <App />
-          </AuthProvider>
+          </AuthProviderProvider>
         </QueryBoundaries>
         <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
       </QueryClientProvider>
