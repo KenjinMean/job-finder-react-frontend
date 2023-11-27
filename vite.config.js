5ab690e278a9475260e711023e339a28a6875c73
Jin <genesisjinsaquibal@gmail.com>
2023-11-27 23:30:26 +0800

    chore: -Comment out auto-deployment workflow - comment out base url for local development

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/job-finder-react-frontend/",
+  // deploy source https://github.com/ErickKS/vite-react-router
+  // base: "/job-finder-react-frontend/",
 });
