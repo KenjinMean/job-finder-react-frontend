f3f5d93dc0680bbd0b67a32c4fb533ab2a260c1c
Jin <genesisjinsaquibal@gmail.com>
2023-12-06 14:46:06 +0800

    added: new page routes for managing user skills

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,16 +1,49 @@
+// NOTE when using this commented implementation the routes should be accessed using the routes object e.g. (routes.jobListingPageRoute)
+// export const baseUrl = "/job-finder-react-frontend/";
+
+// const prependBaseUrl = (route) => baseUrl + route;
+
+// const routeNames = {
+//   jobListingPage: "jobs",
+//   jobDetailsPage: "jobs/view/",
+//   jobSearchResultPage: "jobs/search",
+
+//   userProfilePage: "profile",
+//   userAddSkillPage: "profile/add-skill",
+//   userEditSkillPage: "profile/edit-skills",
+//   userAddSkillSuccessPage: "profile/add-skill-success",
+//   userAddSkillErrorPage: "profile/add-skill-error",
+//   userAddSkillStatusPage: "profile/add-skill-status/",
+
+//   authLoginPage: "auth/login",
+//   authRegisterPage: "auth/register",
+//   authProviderCallbackPage: "auth-provider-callback",
+// };
+
+// export const routes = Object.fromEntries(
+//   Object.entries(routeNames).map(([key, value]) => [key, prependBaseUrl(value)])
+// );
+
 const baseUrl = "/job-finder-react-frontend/";
 
-const jobListingPageRoute = `${baseUrl}jobs`;
-const jobDetailsPageRoute = `${baseUrl}jobs/view/`;
-const jobSearchResultPageRoute = `${baseUrl}jobs/search`;
+const prependBaseUrl = (route) => baseUrl + route;
 
-const userProfilePageRoute = `${baseUrl}profile`;
-const userAddSkillPageRoute = `${baseUrl}profile/add-skill`;
-const userEditSkillPageRoute = `${baseUrl}profile/edit-skills`;
+const jobListingPageRoute = prependBaseUrl("jobs");
+const jobDetailsPageRoute = prependBaseUrl("jobs/view/");
+const jobSearchResultPageRoute = prependBaseUrl("jobs/search");
 
-const authLoginPageRoute = `${baseUrl}auth/login`;
-const authRegisterPageRoute = `${baseUrl}auth/register`;
-const authProviderCallbackPageRoute = `${baseUrl}auth-provider-callback`;
+const userProfilePageRoute = prependBaseUrl("profile");
+const userAddSkillPageRoute = prependBaseUrl("profile/add-skill");
+const userEditSkillPageRoute = prependBaseUrl("profile/edit-skills");
+const userAddSkillSuccessPageRoute = prependBaseUrl(
+  "profile/add-skill-success"
+);
+const userAddSkillErrorPageRoute = prependBaseUrl("profile/add-skill-error");
+const userAddSkillStatusPageRoute = prependBaseUrl("profile/add-skill-status/");
+
+const authLoginPageRoute = prependBaseUrl("auth/login");
+const authRegisterPageRoute = prependBaseUrl("auth/register");
+const authProviderCallbackPageRoute = prependBaseUrl("auth-provider-callback");
 
 export {
   baseUrl,
@@ -23,4 +56,7 @@ export {
   authLoginPageRoute,
   authRegisterPageRoute,
   authProviderCallbackPageRoute,
+  userAddSkillSuccessPageRoute,
+  userAddSkillErrorPageRoute,
+  userAddSkillStatusPageRoute,
 };
