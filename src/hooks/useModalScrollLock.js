7636ad3d8fb53dcd3a8161cfdf66f20279f6eed7
Jin <genesisjinsaquibal@gmail.com>
2023-12-06 14:51:31 +0800

    docs: -Document useModalScrollLock hook for Scroll Management in Modals - "Document useModalScrollLock hook for Scroll Management in Modals
    Added comprehensive documentation for the useModalScrollLock hook, outlining its purpose, functions, and usage examples in both modal and parent components.

--- src/hooks/useModalScrollLock.js
+++ src/hooks/useModalScrollLock.js
@@ -1,54 +1,97 @@
 // INSPIRATION https://blog.logrocket.com/building-react-modal-module-with-react-router/ on "Preventing the scroll underneath the modal" section
-import { useEffect } from "react";
+
+/**
+ * A custom React hook for managing scroll locking when a modal is active.
+ *
+ * This hook provides functions to disable body scroll when a modal is active
+ * and set a parent ref to control scroll locking on the modal's root/parent component.
+ *
+ * @returns {{
+ *   setElementToScrollLockRef: (ref: React.RefObject<HTMLElement>) => void,
+ *   setScrollLockActive: () => void
+ * }}
+ * - `setElementToScrollLockRef`: Function to set the parent ref of the modal component.
+ * - `setScrollLockActive`: Function to disable body scroll when the modal is active.
+ *
+ * @example
+ * ------- Usage in your modal component --------
+ *
+ * //access setScrollLockActive from useModalScrollLock
+ *  const { setScrollLockActive } = useModalScrollLock();
+ *
+ * // using conditional statement
+ *  if (modalOpen) {
+ *    setScrollLockActive();
+ *  }
+ *
+ * ------- Usage in the root/parent component of the modal --------
+ *
+ * // declare a ref to track root/parent element
+ *  const targetElementRef = useRef();
+ *
+ * // access setElementToScrollLockRef
+ *  const { setElementToScrollLockRef } = useModalScrollLock();
+ *
+ * //pass the root/parent element ref to the setElementToScrollLockRef
+ *  setElementToScrollLockRef(targetElementRef);
+ *
+ */
+
 import { create } from "zustand";
+import { useEffect } from "react";
 import { devtools } from "zustand/middleware";
 import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 
 const useModalScrollLockRef = create(
   devtools((set) => ({
-    modalScrollLockRef: null,
-    setModalScrollLockRef: (ref) => set({ modalScrollLockRef: ref }),
+    targetElementRef: null,
+    setTargetElementRef: (ref) => set({ targetElementRef: ref }),
   }))
 );
 
 export function useModalScrollLock() {
-  const { modalScrollLockRef, setModalScrollLockRef } = useModalScrollLockRef();
+  const { targetElementRef, setTargetElementRef } = useModalScrollLockRef();
 
   /**
-
-    call this function to the modal where you want to disable
-    the scroll of the parent component when modal is active/openend
-    
+    call this function to the modal component where you want to disable
+    the scroll of the root/parent component when modal is active/openend
   */
-  const setScrollLockOnModalActive = () => {
+  const setScrollLockActive = () => {
     useEffect(() => {
-      const observerRefValue = modalScrollLockRef.current;
-      disableBodyScroll(observerRefValue);
+      const targetElement = targetElementRef.current;
+      disableBodyScroll(targetElement);
+
+      // cleanup function to restore scroll on the parent component
       return () => {
-        if (observerRefValue) {
-          enableBodyScroll(observerRefValue);
+        if (targetElement) {
+          enableBodyScroll(targetElement);
         }
       };
-    }, [modalScrollLockRef]);
+    }, [targetElementRef]);
   };
 
   /**
-
-    call this function to parent of modal with ref of the component to
-    where you want the scroll to lock when the modal is open
-
-    const modalScrollLockRef = useRef()
-    setModalParentRef(modalScrollLockRef)
-    
-  */
-  const setModalParentRef = (ref) => {
+   * Call this function in the parent component or root element
+   * with a ref to the component where you want the scroll to lock when the modal is open.
+   *
+   * // declare a useRef
+   * const elementToScrollLockRef = useRef();
+   *
+   * // access setElementToScrollLockRef from useModalScrollLock
+   * const { setElementToScrollLockRef } = useModalScrollLock();
+   *
+   * // pass the element ref
+   * setElementToScrollLockRef(elementToScrollLockRef);
+   */
+  const setElementToScrollLockRef = (ref) => {
+    //NOTE: results in infinite loop if not inside useEffect
     useEffect(() => {
-      setModalScrollLockRef(ref);
+      setTargetElementRef(ref);
     }, []);
   };
 
   return {
-    setModalParentRef,
-    setScrollLockOnModalActive,
+    setElementToScrollLockRef,
+    setScrollLockActive,
   };
 }
