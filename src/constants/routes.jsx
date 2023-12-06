9be1b6a629f32aa30870ae3b16c2b8b867dcb538
Jin <genesisjinsaquibal@gmail.com>
2023-12-06 21:27:23 +0800

    refactor: directly export the routes

--- src/constants/routes.jsx
+++ src/constants/routes.jsx
@@ -24,39 +24,30 @@
 //   Object.entries(routeNames).map(([key, value]) => [key, prependBaseUrl(value)])
 // );
 
-const baseUrl = import.meta.env.VITE_APP_BASE_URL;
+// IMPROVE: dynamic routes can be a function that accepts the dynamic variable and return it.
+export const baseUrl = import.meta.env.VITE_APP_BASE_URL;
 
-const prependBaseUrl = (route) => baseUrl + route;
+export const prependBaseUrl = (route) => baseUrl + route;
 
-const jobListingPageRoute = prependBaseUrl("jobs");
-const jobDetailsPageRoute = prependBaseUrl("jobs/view/");
-const jobSearchResultPageRoute = prependBaseUrl("jobs/search");
+export const jobListingPageRoute = prependBaseUrl("jobs");
+export const jobDetailsPageRoute = prependBaseUrl("jobs/view/");
+export const jobSearchResultPageRoute = prependBaseUrl("jobs/search");
 
-const userProfilePageRoute = prependBaseUrl("profile");
-const userAddSkillPageRoute = prependBaseUrl("profile/add-skill");
-const userEditSkillPageRoute = prependBaseUrl("profile/edit-skills");
-const userAddSkillSuccessPageRoute = prependBaseUrl(
+export const userProfilePageRoute = prependBaseUrl("profile");
+export const userAddSkillPageRoute = prependBaseUrl("profile/add-skill");
+export const userEditSkillPageRoute = prependBaseUrl("profile/edit-skills");
+export const userAddSkillSuccessPageRoute = prependBaseUrl(
   "profile/add-skill-success"
 );
-const userAddSkillErrorPageRoute = prependBaseUrl("profile/add-skill-error");
-const userAddSkillStatusPageRoute = prependBaseUrl("profile/add-skill-status/");
+export const userAddSkillErrorPageRoute = prependBaseUrl(
+  "profile/add-skill-error"
+);
+export const userAddSkillStatusPageRoute = prependBaseUrl(
+  "profile/add-skill-status/"
+);
 
-const authLoginPageRoute = prependBaseUrl("auth/login");
-const authRegisterPageRoute = prependBaseUrl("auth/register");
-const authProviderCallbackPageRoute = prependBaseUrl("auth-provider-callback");
-
-export {
-  baseUrl,
-  jobListingPageRoute,
-  jobDetailsPageRoute,
-  jobSearchResultPageRoute,
-  userProfilePageRoute,
-  userAddSkillPageRoute,
-  userEditSkillPageRoute,
-  authLoginPageRoute,
-  authRegisterPageRoute,
-  authProviderCallbackPageRoute,
-  userAddSkillSuccessPageRoute,
-  userAddSkillErrorPageRoute,
-  userAddSkillStatusPageRoute,
-};
+export const authLoginPageRoute = prependBaseUrl("auth/login");
+export const authRegisterPageRoute = prependBaseUrl("auth/register");
+export const authProviderCallbackPageRoute = prependBaseUrl(
+  "auth-provider-callback"
+);
