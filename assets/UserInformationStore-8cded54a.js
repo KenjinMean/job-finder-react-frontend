a94e9748823f6c2d8d6ef9a9a6e45eff8ae46447
KenjinMean <KenjinMean@users.noreply.github.com>
2023-12-06 07:15:29 +0000

    deploy: 329af4bafbb694da63a9c0c083a26570b53ce797

---
+++ assets/UserInformationStore-8cded54a.js
@@ -0,0 +1,1 @@
+import{x as r,y as i}from"./index-6538b809.js";const o=r(i(t=>({user:{user_info:{},user_skills:[]},setUser:s=>{t(e=>({...e,user:s}))},userStates:{isAddingSkills:!1,isDeletingSkills:!1,isUpdatingInfo:!1,isUpdatingContact:!1},setUserStates:(s,e)=>t(a=>({userStates:{...a.userStates,[s]:e}}))})));export{o as u};
