92369dfc3558af091ac9cda75cdde91ceea03996
Jin <genesisjinsaquibal@gmail.com>
2023-12-09 20:35:00 +0800

    refactor: encapsulate CSP configuration logic
    Extracted the Content Security Policy (CSP) configuration logic into a
    reusable function, generateCSPConfig, improving modularity and readability.

--- src/config/cspConfig.js
+++ src/config/cspConfig.js
@@ -1,15 +1,19 @@
 // IMPROVE: define csp in http headers
+const generateCSPConfig = (configContent) => {
+  return Object.entries(configContent)
+    .map(([directive, value]) => `${directive} ${value}`)
+    .join("; ");
+};
 
 const cspConfigContent = {
   defaultSrc: "'self'",
   scriptSrc: "'self' 'unsafe-inline' https://cdn.example.com",
   styleSrc: "'self' 'unsafe-inline' https://fonts.googleapis.com",
   imgSrc:
-    "'self' data: https://example.com https://postsrc.com/ http://127.0.0.1:8000/  http://127.0.0.1:8000/api/user-infos/show http://127.0.0.1:8000/api/jobs/get-job-posting https://via.placeholder.com/",
+    "'self' data: https://example.com https://postsrc.com/ http://127.0.0.1:8000/ https://via.placeholder.com/",
   fontSrc: "'self' https://fonts.gstatic.com",
-  connectSrc:
-    "'self' http://127.0.0.1:8000/ http://127.0.0.1:8000/api/refresh-token http://127.0.0.1:8000/api/user-infos/show http://127.0.0.1:8000/api/jobs/get-job-posting",
+  connectSrc: "'self' http://127.0.0.1:8000/",
 };
 
-const cspConfig = `default-src ${cspConfigContent.defaultSrc}; script-src ${cspConfigContent.scriptSrc}; style-src ${cspConfigContent.styleSrc}; img-src ${cspConfigContent.imgSrc}; font-src ${cspConfigContent.fontSrc}; connect-src ${cspConfigContent.connectSrc};`;
+const cspConfig = generateCSPConfig(cspConfigContent);
 export default cspConfig;
