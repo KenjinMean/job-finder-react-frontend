3d93146682333784e8edaaf85486973a65a72997
Jin <genesisjinsaquibal@gmail.com>
2023-12-10 19:57:45 +0800

    refactor: removed rotue protection guard - removed the code that redirects user to auth page if not authenticated. - redirections is already handled by ProtectedRouteUtilComponent - removed related imports

--- src/layouts/User.Layout.jsx
+++ src/layouts/User.Layout.jsx
@@ -1,22 +1,11 @@
 import React, { Fragment } from "react";
 import { Outlet } from "react-router-dom";
-import { Navigate } from "react-router-dom";
-
-import { authLoginPageRoute } from "../constants/routes.jsx";
-
-import { useAuthenticationStore } from "../services/state/AuthenticationStore";
 
 import HeaderComponent from "../components/header/Header.Component";
 
 import MaxWidthWrapperUtil from "../components/utils/MaxWidthWrapper.Util";
 
 export default function UserProfileLayout() {
-  const { token } = useAuthenticationStore();
-
-  if (!token) {
-    return <Navigate to={authLoginPageRoute} />;
-  }
-
   return (
     <Fragment>
       <HeaderComponent />
