66d9affbc5eb5199850addcf8c979f881be4cc8e
Jin <genesisjinsaquibal@gmail.com>
2023-12-11 21:39:07 +0800

    feat: added ToasContainer

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import "./index.css";
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { QueryBoundaries } from "./components/utils/QueryBoundaries.Util";
@@ -25,6 +27,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <QueryBoundaries>
         <AuthProviderProvider>
           <App />
+          <ToastContainer />
         </AuthProviderProvider>
       </QueryBoundaries>
       <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
