222b0c1c9c9946cf4e835279cedd02cc37b4f06d
Jin <genesisjinsaquibal@gmail.com>
2023-12-11 21:44:06 +0800

    add: -new api fetch custom hooks - useAsyncRemoveUserSkill - useFetchUserSkills - fixed typo on removeUserSkill from removeUSerSkill

--- src/services/api/useSkillRequestHandler.js
+++ src/services/api/useSkillRequestHandler.js
@@ -4,8 +4,8 @@ import {
   userAddSkillErrorPageRoute,
 } from "../../constants/routes.jsx";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useAuthenticationStore } from "../state/AuthenticationStore";
 import { useNavigate } from "react-router-dom";
+import { useAuthenticationStore } from "../state/AuthenticationStore";
 
 const searchSkill = (keyword) => {
   return axiosClient.get(`/search-skills?keyword=${keyword}`);
@@ -15,8 +15,12 @@ const addUserSkill = (skillId) => {
   return axiosClient.post(`/add-skill?skill_id=${skillId}`);
 };
 
-const removeUSerSkill = (skillId) => {
+const removeUserSkill = (skillId) => {
   return axiosClient.delete(`/remove-skill?skill_id=${skillId}`);
+};
+
+const fetchUserSkill = () => {
+  return axiosClient.get(`/get-user-skills`);
 };
 
 export const useSearchSkill = (keyword, setKeyword) => {
@@ -24,7 +28,6 @@ export const useSearchSkill = (keyword, setKeyword) => {
     queryKey: ["searchskill"],
     queryFn: async () => {
       const response = await searchSkill(keyword);
-
       return response;
     },
     select: (data) => data?.data?.skills,
@@ -38,7 +41,6 @@ export const useAddUserSkill = () => {
 
   return useMutation(addUserSkill, {
     onSuccess: async () => {
-      await queryClient.refetchQueries(["userinfo", authenticatedUser.id]);
       queryClient.invalidateQueries("searchskill");
       navigate(userAddSkillSuccessPageRoute);
     },
@@ -53,12 +55,46 @@ export const useAddUserSkill = () => {
   });
 };
 
+// remove skill using useMutation
 export const useRemoveUserSkill = () => {
   const navigate = useNavigate();
 
-  return useMutation(removeUSerSkill, {
+  return useMutation(removeUserSkill, {
     onSuccess: async () => {
       navigate(userAddSkillSuccessPageRoute);
     },
+  });
+};
+
+// remove skill using an async function, used for react-toast that needs an async function to work
+export const useAsyncRemoveUserSkill = () => {
+  const { authenticatedUser } = useAuthenticationStore();
+  const queryClient = useQueryClient();
+
+  return async (skillId) => {
+    try {
+      const response = await axiosClient.delete(
+        `/remove-skill?skill_id=${skillId}`
+      );
+      if (response.status === 200) {
+        queryClient.refetchQueries(["fetchUserSkills", authenticatedUser.id]);
+      }
+    } catch (error) {
+      console.error("Error removing skill:", error);
+      throw error;
+    }
+  };
+};
+
+export const useFetchUserSkills = (userId) => {
+  const { authenticatedUser } = useAuthenticationStore();
+  return useQuery({
+    queryKey: ["fetchUserSkills", authenticatedUser.id],
+    queryFn: async () => {
+      const response = await fetchUserSkill();
+      return response;
+    },
+    select: (data) => data?.data?.skills,
+    suspense: true,
   });
 };
