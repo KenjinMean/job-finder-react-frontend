d5811ceec8ecfa9a475864754519d2c3c6bae683
Jin <genesisjinsaquibal@gmail.com>
2023-12-12 20:38:48 +0800

    refactor: -centralized userInfos into a single component from userProfile Component -

--- src/components/user/UserInfo.Component.jsx
+++ src/components/user/UserInfo.Component.jsx
@@ -1,19 +1,35 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-export default function UserInfoComponent({ userData, handleEdit }) {
+import { useFetchtUserInfo } from "../../services/api/useProfileRequesthandler";
+
+import LinkEditUiComponent from "../UI/LinkEdit.Ui.Component";
+import ClickableLinkedImageUiComponent from "../UI/ClickableLinkedImage.Ui.Component";
+
+export default function UserInfoComponent() {
+  const { data: userData } = useFetchtUserInfo();
+
   return (
-    <div className="relative p-5">
-      <div className="flex gap-1 mt-10 text-xl font-bold">
-        <p>{userData?.firstName}</p>
-        <p>{userData?.lastName}</p>
+    <section className="relative w-full overflow-hidden rounded-lg bg-slate-200">
+      <ClickableLinkedImageUiComponent
+        imagePathUrl={userData?.cover_image}
+        to="/edit-cover"
+        className="block w-full h-48"
+      />
+      <ClickableLinkedImageUiComponent
+        imagePathUrl={userData?.profile_image}
+        to="/edit-profile"
+        className="absolute w-40 h-40 overflow-hidden border-4 rounded-full top-20 left-5 border-slate-200"
+      />
+      <div className="relative p-5">
+        <div className="flex gap-1 mt-10 text-xl font-bold">
+          <p>{userData?.firstName}</p>
+          <p>{userData?.lastName}</p>
+        </div>
+        <p>{userData?.headline}</p>
+        <span>{userData?.location}</span>
+        <p>{userData?.about}</p>
+        <LinkEditUiComponent className="absolute right-5 top-5" />
       </div>
-      <p>{userData?.headline}</p>
-      <span>{userData?.location}</span>
-      <p>{userData?.about}</p>
-      <Link className="absolute right-5 top-5" to="/profile/setup">
-        edit
-      </Link>
-    </div>
+    </section>
   );
 }
--- src/components/user/UserProfile.Component.jsx
+++ src/components/user/UserProfile.Component.jsx
@@ -1,55 +1,17 @@
 import React, { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 
-import { useAuthenticationStore } from "../../services/state/AuthenticationStore";
-import { useUserInformationStore } from "../../services/state/UserInformationStore";
-import { useFetchtUserInfo } from "../../services/api/useProfileRequesthandler";
-
 import UserInfoComponent from "./UserInfo.Component";
 import UserSkillsComponent from "./UserSkills.Component";
-import ClickableLinkedImageUiComponent from "../UI/ClickableLinkedImage.Ui.Component";
-
-import { PageTitleUtil } from "../../components/utils/PageTitle.Util";
 
 export default function UserProfileComponent() {
-  const { user, setUser } = useUserInformationStore();
-  const { authenticatedUser } = useAuthenticationStore();
-
-  useFetchtUserInfo(authenticatedUser.id, setUser);
-
-  const handleEdit = () => {
-    setEditActive((prev) => !prev);
-  };
-
   return (
     <Fragment>
-      <PageTitleUtil title="Profile" />
-      <div>
-        <section className="relative w-full overflow-hidden rounded-lg bg-slate-200">
-          <ClickableLinkedImageUiComponent
-            imagePathUrl={user?.user_info?.cover_image}
-            to="/edit-cover"
-            className="block w-full h-48"
-          />
-          <ClickableLinkedImageUiComponent
-            imagePathUrl={user?.user_info?.profile_image}
-            to="/edit-profile"
-            className="absolute w-40 h-40 overflow-hidden border-4 rounded-full top-20 left-5 border-slate-200"
-          />
+      <UserInfoComponent />
+      <UserSkillsComponent />
 
-          <div className="">
-            <UserInfoComponent
-              userData={user.user_info}
-              handleEdit={handleEdit}
-            />
-            <div></div>
-          </div>
-        </section>
-        <UserSkillsComponent userData={user} />
-
-        {/* This Outlet renders modals matched from url */}
-        <Outlet />
-      </div>
+      {/* This Outlet renders modals matched from url */}
+      <Outlet />
     </Fragment>
   );
 }
