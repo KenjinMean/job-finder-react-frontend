7d96101869b5f6beda256339c6a70c6b710f34a2
Jin <genesisjinsaquibal@gmail.com>
2023-12-13 20:37:40 +0800

    bug fix: Link's preventScrollReset is not accepted -passed ...restProps instead of preventScrollReset

--- src/components/UI/LinkActionPrimary.Ui.Component.jsx
+++ src/components/UI/LinkActionPrimary.Ui.Component.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 export default function LinkActionPrimaryUiComponent({
   to,
   children,
-  preventScrollReset,
+  ...restProps
 }) {
   return (
     <Link
       className="px-4 py-1 text-white bg-indigo-500 rounded-full font-secondary hover:bg-indigo-600"
       to={to}
-      preventScrollReset={preventScrollReset}
+      {...restProps}
     >
       {children}
     </Link>
