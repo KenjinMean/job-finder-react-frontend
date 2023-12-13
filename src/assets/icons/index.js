ba946cddadec3ff2a6a5ecc7235f3cbd1493ce64
Jin <genesisjinsaquibal@gmail.com>
2023-12-13 11:00:26 +0800

    feat: created index.js for icons for centralized exports

---
+++ src/assets/icons/index.js
@@ -0,0 +1,28 @@
+import successIconPath from "./success.png";
+import pencilIconPath from "./edit3.png";
+import addIconPath from "./add.png";
+import closeIconPath from "./cross.png";
+
+export const addIcon = {
+  path: addIconPath,
+  attribution:
+    "Plus icons created by Freepik - Flaticon https://www.flaticon.com/free-icons/plus",
+};
+
+export const successIcon = {
+  path: successIconPath,
+  attribution:
+    "Success icons created by Talha Dogar - Flaticon https://www.flaticon.com/free-icons/success",
+};
+
+export const pencilIcon = {
+  path: pencilIconPath,
+  attribution:
+    "Pencil icons created by Pixel perfect https://www.flaticon.com/free-icons/pencil",
+};
+
+export const closeIcon = {
+  path: closeIconPath,
+  attribution:
+    "Close icons created by vectaicon - Flaticon https://www.flaticon.com/free-icons/close",
+};
