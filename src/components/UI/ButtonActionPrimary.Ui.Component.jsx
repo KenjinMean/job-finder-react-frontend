eccc7f314ac7047e080661b04eb01ba3b9407aa1
Jin <genesisjinsaquibal@gmail.com>
2023-12-19 13:17:19 +0800

    chore: spread restprops on button component to handle multiple props

--- src/components/UI/ButtonActionPrimary.Ui.Component.jsx
+++ src/components/UI/ButtonActionPrimary.Ui.Component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-export default function ButtonActionPrimaryUiComponent({ onClick, children }) {
+export default function ButtonActionPrimaryUiComponent({
+  children,
+  ...restProps
+}) {
   return (
     <button
       className="px-4 py-1 text-white bg-indigo-500 rounded-full font-secondary hover:bg-indigo-600"
-      onClick={onClick}
+      {...restProps}
     >
       {children}
     </button>
