58894795b1333ed08a012d44cdf9cc3cd2180884
KenjinMean <KenjinMean@users.noreply.github.com>
2023-12-27 13:20:58 +0000

    deploy: 4b73a180a0466e0d70bff177927e87abb336d70d

--- assets/LinkActionPrimary.Ui.Component-1ece5b42.js
+++
@@ -1,1 +0,0 @@
-import{j as r,L as s}from"./index-59cb0d5d.js";function a({to:n,children:o,...i}){const{...t}=i;return r.jsx(s,{className:"px-4 py-1 text-white bg-indigo-500 rounded-full font-secondary hover:bg-indigo-600",to:n,...t,children:o})}export{a as L};
