32e7b9f31212dbc65e2b2531827768d82d9491e2
Jin <genesisjinsaquibal@gmail.com>
2023-12-28 18:48:47 +0800

    chore: removed lazy loading on profile/cover preview pages

--- src/pages/UserCoverImageUpdatePreview.Page.jsx
+++ src/pages/UserCoverImageUpdatePreview.Page.jsx
@@ -1,19 +1,12 @@
-import React, { Fragment, Suspense, lazy } from "react";
+import React, { Fragment } from "react";
 import { PageTitleUtil } from "../components/utils/PageTitle.Util";
-
-const UserCoverImageUpdatePreviewModalComponent = lazy(() =>
-  import(
-    "../components/modals/user/UserCoverImageUpdatePreview.Modal.Component"
-  )
-);
+import UserCoverImageUpdatePreviewModalComponent from "../components/modals/user/UserCoverImageUpdatePreview.Modal.Component";
 
 export default function UserCoverImageUpdatePreviewPage() {
   return (
     <Fragment>
       <PageTitleUtil title="Cover Image Preview" />
-      <Suspense fallback={<div>loading...</div>}>
-        <UserCoverImageUpdatePreviewModalComponent />
-      </Suspense>
+      <UserCoverImageUpdatePreviewModalComponent />
     </Fragment>
   );
 }
--- src/pages/UserProfileImageUpdatePreview.Page.jsx
+++ src/pages/UserProfileImageUpdatePreview.Page.jsx
@@ -1,19 +1,12 @@
-import React, { Fragment, Suspense, lazy } from "react";
+import React, { Fragment } from "react";
 import { PageTitleUtil } from "../components/utils/PageTitle.Util";
-
-const UserProfileImageUpdatePreviewModalComponent = lazy(() =>
-  import(
-    "../components/modals/user/UserProfileImageUpdatePreview.Modal.Component"
-  )
-);
+import UserProfileImageUpdatePreviewModalComponent from "../components/modals/user/UserProfileImageUpdatePreview.Modal.Component";
 
 export default function UserProfileImageUpdatePreviewPage() {
   return (
     <Fragment>
       <PageTitleUtil title="Image Preview" />
-      <Suspense fallback={<div>loading...</div>}>
-        <UserProfileImageUpdatePreviewModalComponent />
-      </Suspense>
+      <UserProfileImageUpdatePreviewModalComponent />
     </Fragment>
   );
 }
