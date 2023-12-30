feb485e68d7e8b6ecf5a51adf00da6297ad2055d
Jin <genesisjinsaquibal@gmail.com>
2023-12-30 13:40:44 +0800

    feat: created a seperate backdrop util

---
+++ src/components/utils/Backdrop.Util.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+export default function BackdropUtil({ onClick }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 0.5 }}
+      exit={{ opacity: 0 }}
+      onClick={onClick}
+      className="fixed inset-0 z-40 bg-black opacity-25"
+    ></motion.div>
+  );
+}
