a0f6bdad114e5fe713375a38a2fc932f69953cd0
Jin <genesisjinsaquibal@gmail.com>
2024-01-04 14:58:17 +0800

    modified

--- src/components/user/UserProfile.Component.jsx
+++ src/components/user/UserProfile.Component.jsx
@@ -4,16 +4,22 @@ import { Outlet } from "react-router-dom";
 import UserInfoComponent from "./UserInfo.Component";
 import UserSkillsComponent from "./UserSkills.Component";
 import UserContactComponent from "./UserContact.Component";
+import UserAboutComponent from "./UserAbout.Component";
+import UserExperienceComponent from "./UserExperience.Component";
+import UserEducationComponent from "./UserEducation.Component";
 
 export default function UserProfileComponent() {
   return (
     <Fragment>
-      <div className="flex flex-col gap-2 sm:gap-5">
+      <div className="flex flex-col gap-1 sm:gap-2">
         <UserInfoComponent />
+        <UserAboutComponent />
         <UserContactComponent />
+        <UserExperienceComponent />
+        <UserEducationComponent />
         <UserSkillsComponent />
-        {/* This Outlet renders modals matched from url */}
       </div>
+      {/* This Outlet renders modals matched from url */}
       <Outlet />
     </Fragment>
   );
