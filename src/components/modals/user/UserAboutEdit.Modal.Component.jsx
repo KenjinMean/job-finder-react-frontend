5d1471587d6e51ba2727825c1dfc18f8a1a18ce7
Jin <genesisjinsaquibal@gmail.com>
2024-01-04 14:57:52 +0800

    chore: implemented function for updating the user about

--- src/components/modals/user/UserAboutEdit.Modal.Component.jsx
+++ src/components/modals/user/UserAboutEdit.Modal.Component.jsx
@@ -1,12 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import ModalUtil from "../../utils/Modal.Util";
 import { userRoutes } from "../../../constants/routes";
+import {
+  useAsyncUpdateUserInfo,
+  useFetchtUserInfo,
+} from "../../../services/api/useProfileRequesthandler";
+import { toast } from "react-toastify";
 
 export default function UserAboutEditModalComponent() {
-  const [infoChanged, setInfoChanged] = useState(false);
+  const { data: userInfo } = useFetchtUserInfo();
+  const updateUserInfo = useAsyncUpdateUserInfo();
 
   const [payload, setPayload] = useState();
+  const [infoChanged, setInfoChanged] = useState(false);
+
+  const handleInputChange = (e) => {
+    setInfoChanged(true);
+    setPayload({
+      ...payload,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+
+    // method spoofing: appending patch method to simulate patch request
+    formData.append("_method", "PATCH");
+    for (const key in payload) {
+      formData.append(key, payload[key]);
+    }
+
+    toast.promise(updateUserInfo(formData), {
+      pending: "Updating User About",
+      success: "User About updated sucessfully",
+      error: "Error Updating About info",
+    });
+  };
+
+  useEffect(() => {
+    const updatedPayload = {};
+    Object.assign(updatedPayload, userInfo);
+    setPayload(updatedPayload);
+  }, [userInfo]);
 
   return (
     <ModalUtil
@@ -14,20 +52,24 @@ export default function UserAboutEditModalComponent() {
       modalTitle="Edit User About"
       navigateToUrlOnClose={userRoutes.userProfilePage}
     >
-      <div className="flex flex-col p-5 overflow-y-auto">
-        <span className="text-sm"> * indicates required</span>
-        <textarea
-          name="about"
-          id=""
-          value={payload}
-          cols="30"
-          rows="10"
-          className="border"
-        ></textarea>
-      </div>
-      <div className="flex flex-row-reverse p-5">
-        <button>Save</button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-col p-5 overflow-y-auto">
+          <span className="text-sm"> * indicates required</span>
+          <textarea
+            autoFocus
+            name="about"
+            id=""
+            value={payload?.about}
+            cols="30"
+            rows="10"
+            className="border"
+            onChange={handleInputChange}
+          ></textarea>
+        </div>
+        <div className="flex flex-row-reverse p-5">
+          <button>Save</button>
+        </div>
+      </form>
     </ModalUtil>
   );
 }
