080f4e3e72ed4af8ba756ea45b392af55650da1b
Jin <genesisjinsaquibal@gmail.com>
2024-01-06 13:47:56 +0800

    updated: removed modal states

--- src/services/state/OverlaysStatesStore.js
+++ src/services/state/OverlaysStatesStore.js
@@ -1,25 +1,9 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
-import { userOverlays } from "../../constants/routes";
 
 export const useOverlaysStatesStore = create(
   devtools((set) => ({
     confirmDialogState: false,
-
-    userModalStates: {
-      [userOverlays.userAboutEditOverlay]: false,
-      [userOverlays.userSkillAddOverlay]: false,
-      [userOverlays.userProfileImagePreviewOverlay]: false,
-      [userOverlays.userInfoEditOverlay]: false,
-    },
-
-    setUserModalStates: (modalStates) =>
-      set((state) => ({
-        userModalStates: {
-          ...state.userModalStates,
-          ...modalStates,
-        },
-      })),
 
     setConfirmDialogState: (modalState) =>
       set((state) => ({
