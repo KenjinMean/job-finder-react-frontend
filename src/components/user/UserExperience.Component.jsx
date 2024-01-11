74853c53470242a3a8a1101015392da3adeb06de
Jin <genesisjinsaquibal@gmail.com>
2024-01-11 21:29:21 +0800

    feat: used not yet implemented when cklicking edit link

--- src/components/user/UserExperience.Component.jsx
+++ src/components/user/UserExperience.Component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import LinkEditUiComponent from "../UI/LinkEdit.Ui.Component";
+import { useOpenDialog } from "../../hooks/useOverlayFunctions";
+import { dialogNames } from "../../constants/DialogNames.Constants";
 
 export default function UserExperienceComponent() {
   return (
@@ -7,7 +9,9 @@ export default function UserExperienceComponent() {
       <h2 className="text-2xl font-semibold">Experience</h2>
 
       <div className="absolute flex items-center gap-1 right-5 top-5">
-        <LinkEditUiComponent />
+        <LinkEditUiComponent
+          onClick={useOpenDialog(dialogNames.notImplementedDialog.name)}
+        />
       </div>
     </section>
   );
