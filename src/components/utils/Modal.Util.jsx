1f9debc3fe65aa628f0b19ff884c9b017bc143ab
Jin <genesisjinsaquibal@gmail.com>
2024-01-13 20:53:55 +0800

    refactor: Refactor modal component for enhanced reusability - This commit addresses the modal component's ability to dynamically   adjust its height. It ensures that the modal can accommodate children   with potential overflow, making the component scrollable.

--- src/components/utils/Modal.Util.jsx
+++ src/components/utils/Modal.Util.jsx
@@ -1,3 +1,4 @@
+// INSPIRATION: Modal Scroll Lock SOURCE: https://blog.logrocket.com/building-react-modal-module-with-react-router/
 // SOURCE of reusability using variants: https://youtu.be/eXRlVpw1SIQ?si=ccH914EaOGqEGgp1
 // SOURCE of animation: https://youtu.be/SuqU904ZHA4?si=CeDPmFmCDPzY6Bnf
 
@@ -18,10 +19,10 @@ import ButtonClosePrimaryUiComponent from "../UI/ButtonClosePrimary.Ui.Component
 const ModalUtil = ({
   size,
   children,
-  position,
   className,
   modalTitle,
   isInputChanged = false,
+  handleCloseModal,
   ...props
 }) => {
   const mountElement = document.getElementById("overlays");
@@ -32,6 +33,14 @@ const ModalUtil = ({
 
   const handleModalClose = useModalExitHandler(isInputChanged);
 
+  // close modal if backdrop is clicked
+  const handleBackdropClick = (e) => {
+    // Check if the clicked element is the backdrop itself, not one of its children
+    if (e.target.classList.contains("modal-backdrop")) {
+      handleModalClose();
+    }
+  };
+
   return createPortal(
     <Fragment>
       <motion.div
@@ -39,23 +48,26 @@ const ModalUtil = ({
         initial="hidden"
         animate="visible"
         exit="exit"
-        className={cn(ModalVariants({ size, position, className }))}
+        onClick={handleBackdropClick}
         {...props}
+        className="fixed inset-0 z-20 flex items-start justify-center overflow-y-auto sm:p-5 modal-backdrop"
       >
-        <div className="flex flex-col w-full bg-white shadow-lg">
-          <div className="flex items-center justify-between p-5 border-b border-slate-300">
-            <h2 className="text-xl">{modalTitle}</h2>
-            <ButtonClosePrimaryUiComponent
-              onClick={handleModalClose}
-              preventScrollReset={false}
-            />
-          </div>
+        <div className={cn(ModalVariants({ size, className }))}>
+          {modalTitle && (
+            <div className="flex items-center justify-between p-5 overflow-hidden">
+              <h2 className="text-xl">{modalTitle}</h2>
+              <ButtonClosePrimaryUiComponent
+                onClick={handleModalClose}
+                preventScrollReset={false}
+              />
+            </div>
+          )}
           {children}
         </div>
       </motion.div>
 
       {/* Backdrop */}
-      <BackdropUtil onClick={handleModalClose} />
+      <BackdropUtil />
     </Fragment>,
     mountElement
   );
