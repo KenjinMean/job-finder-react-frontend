3428dc6752a92882c23c261d400c4d659e71812a
Jin <genesisjinsaquibal@gmail.com>
2024-01-13 21:00:10 +0800

    chore: added userContactModal

--- src/constants/ModalNames.Constants.ts
+++ src/constants/ModalNames.Constants.ts
@@ -2,6 +2,7 @@ import UserInfoEditPage from "../pages/UserInfoEdit.Page"
 import UserAddSkillPage from "../pages/UserAddSkill.Page"
 import UserAboutEditPage from "../pages/UserAboutEdit.Page"
 
+import UserContactEditModalComponent from "../components/modals/user/UserContactEdit.Modal.Component"
 import UserAddSkillErrorModaComponent from "../components/modals/user/UserAddSkillError.Modal.Component"
 import UserCoverImageViewModalComponent from "../components/modals/user/UserCoverImageView.Modal.Component"
 import UserAddSkillSuccessModalComponent from "../components/modals/user/UserAddSkillSuccess.Modal.Component"
@@ -19,22 +20,23 @@ import UserProfileImageUpdatePreviewModalComponent from "../components/modals/us
   });
 
   type UserModal = {
-    userAboutEditModal: { name: string; component: React.ComponentType<any> };
     userAddSkillModal: { name: string; component: React.ComponentType<any> };
     userInfoEditModal: { name: string; component: React.ComponentType<any> };
+    userAboutEditModal: { name: string; component: React.ComponentType<any> };
+    userContactEditModal: { name: string; component: React.ComponentType<any> };
     userAddSkillErrorModal: { name: string; component: React.ComponentType<any> };
     userCoverImageViewModal: { name: string; component: React.ComponentType<any> };
     userAddSkillSuccessModal: { name: string; component: React.ComponentType<any> };
     userProfileImageViewModal: { name: string; component: React.ComponentType<any> };
     userCoverImageUpdatePreviewModal: { name: string; component: React.ComponentType<any> };
     userProfileImageUpdatePreviewModal: { name: string; component: React.ComponentType<any> };
-
   };
   
   const Modals: UserModal = {
-    userAboutEditModal: createModal("userAboutEditModal",UserAboutEditPage),
     userInfoEditModal: createModal("userInfoEditModal",UserInfoEditPage),
     userAddSkillModal: createModal("userAddSkillModal", UserAddSkillPage),
+    userAboutEditModal: createModal("userAboutEditModal",UserAboutEditPage),
+    userContactEditModal: createModal("userContactEditModal",UserContactEditModalComponent ),
     userAddSkillErrorModal: createModal("userAddSkillErrorModal", UserAddSkillErrorModaComponent),
     userCoverImageViewModal: createModal("userCoverImageViewModal", UserCoverImageViewModalComponent),
     userAddSkillSuccessModal: createModal("userAddSkillSuccessModal", UserAddSkillSuccessModalComponent),
