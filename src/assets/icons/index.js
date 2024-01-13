eb9eb62d8f56b11808de59b68c2d2a3e713f5f47
Jin <genesisjinsaquibal@gmail.com>
2024-01-13 20:48:30 +0800

    chore: added new icons for UserContact components

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,6 +2,10 @@ import successIconPath from "./success.png";
 import pencilIconPath from "./edit3.png";
 import addIconPath from "./add.png";
 import closeIconPath from "./cross.png";
+import telephonePath from "./telephone.png";
+import emailPath from "./email.png";
+import locationPath from "./location-icon.png";
+import birthDayPath from "./birthday.png";
 
 export const addIcon = {
   path: addIconPath,
@@ -25,4 +29,28 @@ export const closeIcon = {
   path: closeIconPath,
   attribution:
     "Close icons created by vectaicon - Flaticon https://www.flaticon.com/free-icons/close",
+};
+
+export const telephoneIcon = {
+  path: telephonePath,
+  attribution:
+    "Phone icons created by Gregor Cresnar - Flaticon - https://www.flaticon.com/free-icons/phone",
+};
+
+export const emailIcon = {
+  path: emailPath,
+  attribution:
+    "Email icons created by Freepik - https://www.flaticon.com/free-icons/email",
+};
+
+export const locationIcon = {
+  path: locationPath,
+  attribution:
+    "Email icons created by Freepik - https://www.flaticon.com/free-icons/email",
+};
+
+export const birthDayIcon = {
+  path: birthDayPath,
+  attribution:
+    "Birthday cake icons created by laterunlabs - Flaticon - https://www.flaticon.com/free-icons/birthday-cake",
 };
