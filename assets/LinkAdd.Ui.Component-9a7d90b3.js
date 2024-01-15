e6a78dcba44020283ed741f21b2a4f53cf270253
KenjinMean <KenjinMean@users.noreply.github.com>
2024-01-15 14:26:29 +0000

    deploy: 0aee0e9a23c73c6e54c98c1ad225cdc6db9935b6

---
+++ assets/LinkAdd.Ui.Component-9a7d90b3.js
@@ -0,0 +1,1 @@
+import{j as t,L as r,C as n}from"./index-3c88b30d.js";function e({to:o,className:s,...a}){const{...i}=a;return t.jsx(r,{className:`p-2 transition-all rounded-full hover:bg-slate-300 ${s}`,to:o,...i,children:t.jsx("img",{className:"block w-5 h-5",src:n.path,alt:`Add Icon. Attribution: ${n.attribution}`})})}export{e as L};
