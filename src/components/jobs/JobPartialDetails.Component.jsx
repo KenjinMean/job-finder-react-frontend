15ec4d90b4689dbbc1d60311ec79fdc58171811a
Jin <genesisjinsaquibal@gmail.com>
2024-01-30 20:43:59 +0800

    css: added flex wrap to handle mobile view

--- src/components/jobs/JobPartialDetails.Component.jsx
+++ src/components/jobs/JobPartialDetails.Component.jsx
@@ -7,7 +7,7 @@ export default function JobPartialDetailsComponent({
   companySize,
 }) {
   return (
-    <p className="flex flex-col gap-1 sm:gap-2 md:flex-row text-content-gray empty:hidden">
+    <p className="flex flex-col flex-wrap gap-1 sm:gap-2 md:flex-row text-content-gray empty:hidden">
       {location && (
         <span className="rounded-md sm:px-2 sm:border w-max border-border-100">
           Location: <span className="text-content-slate_500"> {location}</span>
