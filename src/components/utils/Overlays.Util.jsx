3b78e1e389c095f37a1d08dc84371fb7d05def16
Jin <genesisjinsaquibal@gmail.com>
2024-01-30 19:08:03 +0800

    delete: removed unused components

--- src/components/utils/Overlays.Util.jsx
+++
@@ -1,9 +0,0 @@
-// Modal Scroll Lock SOURCE: https://blog.logrocket.com/building-react-modal-module-with-react-router/
-import React from "react";
-import { createPortal } from "react-dom";
-
-export default function OverlaysUtil({ modalComponent }) {
-  const mountElement = document.getElementById("overlays");
-
-  return createPortal(<>{modalComponent && modalComponent}</>, mountElement);
-}
