b939a924c0ff222b13e6f6a21af13ab99a02f8b4
Jin <genesisjinsaquibal@gmail.com>
2024-02-09 21:27:40 +0800

    Fix: label props not used on label element

--- src/components/UI/LabeledDateInput.Ui.Component.jsx
+++ src/components/UI/LabeledDateInput.Ui.Component.jsx
@@ -10,7 +10,7 @@ export default function LabeledDateInputUiComponent({
   return (
     <div>
       <label htmlFor={id} className="block mb-2 font-medium text-content-black">
-        Birth Date
+        {label}
       </label>
       <input
         className="bg-input-gray border border-border-100 text-content-black rounded-lg focus:ring-accent-blue500 focus:border-accent-blue500 block w-full p-2.5 "
