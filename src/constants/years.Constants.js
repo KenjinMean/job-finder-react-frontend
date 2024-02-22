f974642309f0fcb8ccfa4a31bb92d64a1e4940ff
Jin <genesisjinsaquibal@gmail.com>
2024-02-22 18:34:42 +0800

    Added: years Constants

--- src/constants/years.Constants.js
+++ src/constants/years.Constants.js
@@ -1,10 +1,7 @@
 const currentYear = new Date().getFullYear();
 const startYear = 1920;
 
-export const yearsConstants = Array.from(
+export const yearsArray = Array.from(
   { length: currentYear - startYear + 1 },
-  (_, index) => ({
-    value: startYear + index,
-    name: startYear + index,
-  })
+  (_, index) => startYear + index
 );
