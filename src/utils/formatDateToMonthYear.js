26e07df8b367805d10de67ce65d21466095b261a
Jin <genesisjinsaquibal@gmail.com>
2024-02-25 10:04:11 +0800

    Fix: return empty string if date is not provided

--- src/utils/formatDateToMonthYear.js
+++ src/utils/formatDateToMonthYear.js
@@ -1,4 +1,8 @@
 export function formatDateToMonthYear(dateString) {
+  if (!dateString) {
+    return "";
+  }
+
   const options = { year: "numeric", month: "short" };
   const dateObject = new Date(dateString);
   return dateObject.toLocaleDateString("en-US", options);
