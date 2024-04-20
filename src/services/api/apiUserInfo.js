8051558589670ec9302143f34dff8d53c7c12e68
Jin <genesisjinsaquibal@gmail.com>
2024-04-20 21:16:50 +0800

    Chore: stored user userInfoBaseUrl on a variable for centralization and ease of update

--- src/services/api/apiUserInfo.js
+++ src/services/api/apiUserInfo.js
@@ -1,21 +1,23 @@
 import axiosClient from "../../utils/axiosClient.Util";
 
+const userInfoBaseUrl = "/user-info";
+
 /* ----------------------------------------------------------- */
 export const apiUserInfoFetch = async () => {
-  return await axiosClient.get("/user-info");
+  return await axiosClient.get(`${userInfoBaseUrl}`);
 };
 
 /* ----------------------------------------------------------- */
 export const apiUserInfoUpdate = async (payload) => {
-  return await axiosClient.post("/user-info", payload);
+  return await axiosClient.post(`${userInfoBaseUrl}`, payload);
 };
 
 /* ----------------------------------------------------------- */
 export const apiUserProfileImageUpdate = async (payload) => {
-  return await axiosClient.post("/user-info/profile-image", payload);
+  return await axiosClient.post(`${userInfoBaseUrl}/profile-image`, payload);
 };
 
 /* ----------------------------------------------------------- */
 export const apiUserCoverImageUpdate = async (payload) => {
-  return await axiosClient.post("/user-info/cover-image", payload);
+  return await axiosClient.post(`${userInfoBaseUrl}/cover-image`, payload);
 };
