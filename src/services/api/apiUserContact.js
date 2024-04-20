b54611c45f099dfce3012d343ed0704ce578e0aa
Jin <genesisjinsaquibal@gmail.com>
2024-04-20 21:15:56 +0800

    Chore: stored userContactBaseUrl on a variable for ease of updating the baseUrl

--- src/services/api/apiUserContact.js
+++ src/services/api/apiUserContact.js
@@ -1,19 +1,21 @@
 import axiosClient from "../../utils/axiosClient.Util";
 
+const userUserContactbaseUrl = "/user-contact";
+
 /* ----------------------------------------------------------- */
 // fetch user contact
 export const apiUserContactFetch = async () => {
-  return await axiosClient.get("/user-contact");
+  return await axiosClient.get(`${userUserContactbaseUrl}`);
 };
 
 /* ----------------------------------------------------------- */
 // fetch user contact
 export const apiUserContactStore = async (payload) => {
-  return await axiosClient.post("/user-contact", payload);
+  return await axiosClient.post(`${userUserContactbaseUrl}`, payload);
 };
 
 /* ----------------------------------------------------------- */
 // update user contact
 export const apiUserContactUpdate = async (payload) => {
-  return await axiosClient.post("/user-contact", payload);
+  return await axiosClient.patch(`${userUserContactbaseUrl}`, payload);
 };
