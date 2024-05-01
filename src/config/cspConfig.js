8529ba96e07db4c19977ab358bf6f2eedc5a8be9
Jin <genesisjinsaquibal@gmail.com>
2024-05-01 23:46:38 +0800

    Subject: Update APP_URL and csp config to new deployment URL

--- src/config/cspConfig.js
+++ src/config/cspConfig.js
@@ -6,13 +6,13 @@ const generateCSPConfig = (configContent) => {
 };
 
 const cspConfigContent = {
-  "default-src": "'self'",
+  "default-src": "'self' https://job-finder.cloud/",
   "script-src": "'self' 'unsafe-inline' https://cdn.example.com",
   "style-src": "'self' 'unsafe-inline' https://fonts.googleapis.com",
   "img-src":
-    "'self' data: https://example.com https://postsrc.com/ https://apis.google.com/ https://avatars.githubusercontent.com/ https://lh3.googleusercontent.com/ http://127.0.0.1:8000/ https://via.placeholder.com/",
+    "'self' data: https://example.com https://job-finder.cloud/ https://postsrc.com/ https://apis.google.com/ https://avatars.githubusercontent.com/ https://lh3.googleusercontent.com/ http://127.0.0.1:8000/ https://via.placeholder.com/",
   "font-src": "'self' https://fonts.gstatic.com",
-  "connect-src": "'self' http://127.0.0.1:8000/",
+  "connect-src": "'self' http://127.0.0.1:8000/ https://job-finder.cloud/",
 };
 
 const cspConfig = generateCSPConfig(cspConfigContent);
