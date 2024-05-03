ac0a58c7c06645910012c2c3246962b301cd401e
Jin <genesisjinsaquibal@gmail.com>
2024-05-03 22:53:59 +0800

    Subject: updated csp to handle api url

--- src/config/cspConfig.js
+++ src/config/cspConfig.js
@@ -5,14 +5,24 @@ const generateCSPConfig = (configContent) => {
     .join("; ");
 };
 
+// const cspConfigContent = {
+//   "default-src": "'self'",
+//   "script-src": "'self' 'unsafe-inline' https://cdn.example.com",
+//   "style-src": "'self' 'unsafe-inline' https://fonts.googleapis.com",
+//   "img-src":
+//     "'self' data: https://example.com https://postsrc.com/ https://apis.google.com/ https://avatars.githubusercontent.com/ https://lh3.googleusercontent.com/ http://127.0.0.1:8000/ https://via.placeholder.com/",
+//   "font-src": "'self' https://fonts.gstatic.com",
+//   "connect-src": "'self' http://127.0.0.1:8000/",
+// };
+
 const cspConfigContent = {
-  "default-src": "'self' https://job-finder.cloud/",
+  "default-src": "'self' https://api.job-finder.cloud/",
   "script-src": "'self' 'unsafe-inline' https://cdn.example.com",
   "style-src": "'self' 'unsafe-inline' https://fonts.googleapis.com",
   "img-src":
-    "'self' data: https://example.com https://job-finder.cloud/ https://postsrc.com/ https://apis.google.com/ https://avatars.githubusercontent.com/ https://lh3.googleusercontent.com/ http://127.0.0.1:8000/ https://via.placeholder.com/",
+    "'self' data: https://example.com https://api.job-finder.cloud/ https://postsrc.com/ https://apis.google.com/ https://avatars.githubusercontent.com/ https://lh3.googleusercontent.com/ http://127.0.0.1:8000/ https://via.placeholder.com/",
   "font-src": "'self' https://fonts.gstatic.com",
-  "connect-src": "'self' http://127.0.0.1:8000/ https://job-finder.cloud/",
+  "connect-src": "'self' http://127.0.0.1:8000/ https://api.job-finder.cloud/",
 };
 
 const cspConfig = generateCSPConfig(cspConfigContent);
