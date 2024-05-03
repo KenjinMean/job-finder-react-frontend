ee86b4b2b07696dd846be83012691cd14f04ddcf
Jin <genesisjinsaquibal@gmail.com>
2024-05-03 23:22:55 +0800

    Subject: fix api url to have trailing slash on the image src

--- src/config/cspConfig.js
+++ src/config/cspConfig.js
@@ -20,7 +20,7 @@ const cspConfigContent = {
   "script-src": "'self' 'unsafe-inline' https://cdn.example.com",
   "style-src": "'self' 'unsafe-inline' https://fonts.googleapis.com",
   "img-src":
-    "'self' data: https://example.com https://api.job-finder.cloud https://postsrc.com/ https://apis.google.com/ https://avatars.githubusercontent.com/ https://lh3.googleusercontent.com/ http://127.0.0.1:8000/ https://via.placeholder.com/",
+    "'self' data: https://example.com https://api.job-finder.cloud/ https://postsrc.com/ https://apis.google.com/ https://avatars.githubusercontent.com/ https://lh3.googleusercontent.com/ http://127.0.0.1:8000/ https://via.placeholder.com/",
   "font-src": "'self' https://fonts.gstatic.com",
   "connect-src": "'self' http://127.0.0.1:8000/ https://api.job-finder.cloud",
 };
