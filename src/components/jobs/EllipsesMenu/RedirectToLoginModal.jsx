cd12421e1013651c1fcdef2430deb81782009fd9
Jin <genesisjinsaquibal@gmail.com>
2024-07-04 23:03:30 +0800

    subject: enhanced modal style, used mui theme colors

--- src/components/jobs/EllipsesMenu/RedirectToLoginModal.jsx
+++ src/components/jobs/EllipsesMenu/RedirectToLoginModal.jsx
@@ -1,30 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Box, Modal, Typography, Button } from "@mui/material";
-
-const style = {
-  position: "absolute",
-  display: "flex",
-  flexDirection: "column",
-  gap: "1rem",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  borderRadius: ".3rem",
-  boxShadow: 24,
-  p: 4,
-};
+import { Modal, Paper, Typography, Button, useTheme } from "@mui/material";
+import { authRoutes } from "../../../constants/RoutesPath.Constants";
 
 export default function RedirectToLoginModal({ isOpen, setIsModalOpen }) {
   const navigate = useNavigate();
+  const theme = useTheme();
 
   const handleClose = () => setIsModalOpen(false);
 
   const handleNavigateToLogin = () => {
-    navigate("/auth/login");
+    navigate(authRoutes.authLoginPage);
     handleClose();
   };
 
@@ -35,18 +21,40 @@ export default function RedirectToLoginModal({ isOpen, setIsModalOpen }) {
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Login Required
+      <Paper
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 400,
+          display: "flex",
+          flexDirection: "column",
+          gap: theme.spacing(2),
+          padding: theme.spacing(4),
+          backgroundColor: theme.palette.background.paper,
+        }}
+      >
+        <Typography variant="h6" component="h2" id="modal-modal-title">
+          Access Required
         </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          You need to login to save a job.
+        <Typography
+          variant="body1"
+          sx={{ mt: theme.spacing(2) }}
+          id="modal-modal-description"
+        >
+          You need to be logged in to save a job.
         </Typography>
 
-        <Button onClick={handleNavigateToLogin} variant="contained">
-          Go to Login page
+        <Button
+          onClick={handleNavigateToLogin}
+          variant="contained"
+          color="primary" // Use theme's primary color
+          sx={{ width: "100%" }} // Full width button
+        >
+          Log In to Save Job
         </Button>
-      </Box>
+      </Paper>
     </Modal>
   );
 }
