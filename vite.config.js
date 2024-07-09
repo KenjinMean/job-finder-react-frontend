28a462803d937f229b0a7bbfbb467a46f46b5c75
Jin <genesisjinsaquibal@gmail.com>
2024-07-09 21:40:45 +0800

    Add legacy code support to build configuration with ViteJS plugin
    - Integrated legacy code handling into the ViteJS build configuration.
    - Installed and configured the ViteJS plugin for legacy code to ensure
      compatibility with older browsers.
    - Updated `vite.config.js` to include the legacy plugin and adjusted
      build scripts accordingly.
    - Verified that the build process works for both legacy and modern
      environments.
    
    Details:
    The ViteJS plugin for legacy code was added to support older browsers
      that are not fully compatible with modern JavaScript features.
      The configuration changes were tested to ensure that they do not
      disrupt the existing build process and that legacy support functions
      as expected.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,45 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-// import { baseUrl } from "./src/constants/RoutesPath.Constants";
+import { resolve } from "path";
+import compression from "vite-plugin-compression";
+import legacy from "@vitejs/plugin-legacy";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // deploy source https://www.youtube.com/watch?v=uEEj2c3_ydg
-  // base: baseUrl,
+  plugins: [
+    react(),
+    compression({
+      algorithm: "gzip",
+      ext: ".gz",
+      threshold: 10240,
+      deleteOriginFile: false,
+    }),
+    legacy({
+      targets: ["defaults", "not IE 11"], // Adjust as needed
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+      output: {
+        // ManualChunkign soruce: https://stackoverflow.com/questions/68643743/separating-material-ui-in-vite-rollup-as-a-manual-chunk-to-reduce-chunk-size
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
