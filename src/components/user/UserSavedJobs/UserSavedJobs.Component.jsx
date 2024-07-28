af55022f185c7cf758263b674ecd899cb66b796a
Jin <genesisjinsaquibal@gmail.com>
2024-07-28 23:01:11 +0800

    chore: dynamically render a info if user has no saved jobs

--- src/components/user/UserSavedJobs/UserSavedJobs.Component.jsx
+++ src/components/user/UserSavedJobs/UserSavedJobs.Component.jsx
@@ -1,17 +1,20 @@
 import React, { Fragment } from "react";
-
 import JobCardComponent from "../../jobs/JobCard.Component";
 import { useApiUserSaveJobsFetch } from "../../../hooks/useApiUserSaveJobs";
 
 export default function UserSavedJobsComponent() {
-  const { data } = useApiUserSaveJobsFetch();
+  const { data: userSavedJobs } = useApiUserSaveJobsFetch();
 
   return (
     <Fragment>
       <div className="flex flex-col gap-5 p-5 pt-10 md:pt-10">
-        {data?.map((job) => {
-          return <JobCardComponent key={job.id} job={job} />;
-        })}
+        {userSavedJobs?.length > 0 ? (
+          userSavedJobs.map((job) => {
+            return <JobCardComponent key={job.id} job={job} />;
+          })
+        ) : (
+          <div>No saved jobs available.</div>
+        )}
       </div>
     </Fragment>
   );
